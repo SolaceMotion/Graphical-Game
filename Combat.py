c75cae002c03f8f88ee50e35bfa3b5b6956c7471
Ahmed Gelle Ismail <geellle143@gmail.com>
2022-11-02 12:33:56 +0100

    Combat

--- Combat.py
+++ Combat.py
@@ -11,70 +11,76 @@ pygame.init()
 
 class Combat():
     def __init__(self, player: Player, enemy: Enemy):
-        self.player = player
-        self.enemy = enemy
         self.width, self.height = (1200, 800)
-        self.road_w = int(self.width / 2)
-        self.road_mark = int(self.width / 80)
-        self.left_lane = self.width / 2 - self.road_w / 4
-        self.right_lane = self.width / 2 + self.road_w / 4
+        self.road_w = int(self.width // 2)
+        self.road_mark = int(self.width // 80)
+        self.left_lane = int(self.width // 2 - self.road_w // 4)
+        self.right_lane = int(self.width // 2 + self.road_w // 4)
         self.screen = pygame.display.set_mode((self.width, self.height))
+        self.player = player
+        self.player.set_pos((self.width // 2 + self.road_w // 4, int(self.height * 4 //5)))
+        self.enemy = enemy
+        self.enemy.set_pos ((self.left_lane, int(self.height // 5)))
+
 
         self.running = True
 
-        self.tree_loc = self.player.get_pos()
-        self.tree_loc = self.width / 2 + self.road_w / 4, self.height * 0.8
-        self.enemy_boll_loc = self.enemy.get_pos()
-        self.enemy_boll_loc = self.left_lane, self.height * 0.2
+
 
     def draw(self):
         self.screen.fill((100, 250, 150))
-        pygame.draw.rect(self.screen,(155,150,150),(self.width/2 - self.road_w/2, 0, self.road_w, self.height ))
-        pygame.draw.rect(self.screen,(255,255,60),(self.width/2 - self.road_mark/2, 0, self.road_mark, self.height ))
-        pygame.draw.rect(self.screen,(255,255,255),(self.width/2 - self.road_w/2 + self.road_mark*2, 0, self.road_mark, self.height ))
-        pygame.draw.rect(self.screen,(255,255,255),(self.width/2 +  self.road_w/2 - self.road_mark*3, 0, self.road_mark, self.height ))
+        pygame.draw.rect(self.screen,(155,150,150),(self.width//2 - self.road_w//2, 0, self.road_w, self.height ))
+        pygame.draw.rect(self.screen,(255,255,60),(self.width//2 - self.road_mark//2, 0, self.road_mark, self.height ))
+        pygame.draw.rect(self.screen,(255,255,255),(self.width//2 - self.road_w//2 + self.road_mark*2, 0, self.road_mark, self.height ))
+        pygame.draw.rect(self.screen,(255,255,255),(self.width//2 +  self.road_w//2 - self.road_mark*3, 0, self.road_mark, self.height ))
 
 
     def run_Combat(self):
-        a = 1
-        b = 1
+        clock = pygame.time.Clock()
+
+        self.player.points = 1
         while self.running:
-            if self.enemy_boll_loc [0] == self.tree_loc[0] and self.enemy_boll_loc [1] == self.tree_loc[1] - 80:
-                print('GAME OVER, YOU LOST!', 'Game points:', a)
+            clock.tick(60)
+
+            if self.enemy.get_pos() [1] == self.player.get_pos()[1]-50 and self.enemy.get_pos() [0]== self.player.get_pos()[0] :
+                print('GAME OVER, YOU LOST!', 'Game points:', self.player.points)
                 break
-            elif a >= 30:
-                print('congrats you have achieved the highest points')
+            elif self.player.points >= 15:
+                print('Congrats, YOU WON!!!')
                 break
 
             #self.enemy_boll_loc[1] += a
             x, y = self.enemy.get_pos()
-            y +=a
+            y += self.enemy.speed
             self.enemy.set_pos((x, y))
 
-            if self.enemy_boll_loc[1] >= 800:
-                b +=1
-                if b % 3 == 0:
-                    a += 4
-                print (b,a)
-                self.enemy_boll_loc[1] = -100
+            if self.enemy.get_pos()[1] >= 800:
+                self.player.points +=1
+                if self.player.points % 5 == 0:
+                    self.enemy.speed += 2
+                #print (b,a)
+                x, y = self.enemy.get_pos()
+                y =-100
+                self.enemy.set_pos((x, y))
                 lane = random.randint(0,1)
                 if lane == 0:
-                    self.enemy_boll_loc.center = self.left_lane, self.height * 0.2
+                    self.enemy.set_pos((self.left_lane, self.height // 5))
                 else:
-                    self.enemy_boll_loc.center = self.right_lane, self.height * 0.2
+                    self.enemy.set_pos((self.right_lane, self.height // 5))
             for event in pygame.event.get():
                 if event.type == QUIT:
                     self.running = False
                 if event.type == KEYDOWN:
                     if event.key in [K_a, K_LEFT]:
-                        self.tree_loc = self.tree_loc.move([-int(self.road_w/2), 0])
+                        self.player.set_pos((self.left_lane, 600))
                     if event.key in [K_d, K_RIGHT]:
-                        self.tree_loc = self.tree_loc.move([int(self.road_w/2), 0])
-            Combat.draw()
+                        self.player.set_pos((self.right_lane, 600))
+            Combat.draw(self)
 
 
-            self.screen.blit(self.player.get_sprite(),self.tree_loc)
-            self.screen.blit(self.enemy.get_sprite(), self.enemy_boll_loc)
+            self.screen.blit(self.player.get_sprite(),self.player.get_pos())
+            self.screen.blit(self.enemy.get_sprite(), self.enemy.get_pos())
+            pygame.display.update()
             #pygame.display.update()
 
 
--- player.py
+++ player.py
@@ -2,5 +2,6 @@ from character import Character
 
 
 class Player(Character):
-    def __init__(self, sprite: str, *, pos = (100, 100), size = (60, 60)) -> None:
+    def __init__(self, sprite: str, *, pos = (20, 20), size = (60, 60)) -> None:
         super().__init__(sprite, pos = pos, size = size)
+        self.points = 0
