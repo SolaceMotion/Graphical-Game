c18a6267ee69ed8dcf194237bf917cb4ebd77552
TheLuckyLuke1337 <112425988+TheLuckyLuke1337@users.noreply.github.com>
2022-11-04 09:52:37 +0100

    cleanup

--- dungeon_game/boss.py
+++
@@ -1,6 +0,0 @@
-from .enemy import Enemy
-
-
-class Boss(Enemy):
-    def __init__(self, sprite: str, *, pos = (20, 20), size = (60, 60)) -> None:
-        super().__init__(sprite, pos = pos, size = size)
--- dungeon_game/config.py
+++ dungeon_game/config.py
@@ -7,15 +7,13 @@ HEIGHT = RESOLUTION * 9
 PLAYER_SPRITE = "sprites/player.png"
 BOSS_SPRITE = "sprites/boss.png"
 ENEMY_SPRITE = "sprites/enemy.png"
-ENEMY2_SPRITE = "sprites/Enemy_2.png"
+ENEMY2_SPRITE = "sprites/enemy_2.png"
 
 # tile sprites
 BROWN_TILE = "sprites/bgr.tiles.brown.png"
 RED_TILE = "sprites/bgr.tiles.dark_red.png"
-YELLOW_TILE = "sprites/bgr.tiles.dark_yellow.png"
-GREEN_TILE = "sprites/bgr.tiles.green.png"
 
-TILES = {"brown": BROWN_TILE, "red": RED_TILE, "yellow": YELLOW_TILE, "green": GREEN_TILE}
+TILES = {"brown": BROWN_TILE, "red": RED_TILE}
 
 # general settings
 FONT = "ComicSans"
