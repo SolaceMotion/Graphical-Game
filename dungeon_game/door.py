9375f9e2825cc62e3b412aacf019dc5fae60280a
TheLuckyLuke1337 <112425988+TheLuckyLuke1337@users.noreply.github.com>
2022-11-04 11:17:05 +0100

    cleanup

--- dungeon_game/door.py
+++ dungeon_game/door.py
@@ -3,7 +3,7 @@ from .character import Character
 
 class Door(Character):
     def __init__(self, sprite: str, *, size, pos, colidable=True):
-        super().__init__(sprite, pos=pos, size=size, colidable=colidable)
+        super().__init__(sprite, pos=pos, size=size)
         self.__vx = 0
         self.__vy = 0
         self.colidable = colidable
